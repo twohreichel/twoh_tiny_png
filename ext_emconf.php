947f22976b2db7565b0eb1c5815f1864efccdbd4
Andreas Reichel <a.reichel91@outlook.com>
2025-12-07 07:40:59 +0100

    feat(refactoring): increased version number

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF['twoh_tiny_png'] = [
     'title' => 'Tiny PNG',
-    'description' => 'Optimize your TYPO3 Images with Tiny PNG',
+    'description' => 'Optimize your TYPO3 Images using the TinyPNG API',
     'category' => 'plugin',
     'author' => 'Andreas Reichel, Igor Smertin',
     'author_email' => 'a.reichel91@outlook.com, igor.smertin@web.de',
@@ -11,7 +11,7 @@ $EM_CONF['twoh_tiny_png'] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.5',
+    'version' => '1.0.6',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-12.4.99',
